11beed71ae510b6b55514fa817ab9ba7326550bf
Isaac Shapira <fresheyeball@gmail.com>
2014-07-14 21:13:06 -0600

    describe and it are bound

--- tests/Mocha/Mocha.purs
+++ tests/Mocha/Mocha.purs
@@ -4,4 +4,22 @@ import Control.Monad.Eff
 
 foreign import data Describe :: !
 
-foreign import describe :: forall e a. Eff e a -> Eff (describe :: Describe | e) {}
+foreign import describe 
+  " function describe(description){                  \ 
+  \   return function(fn){                           \
+  \     return function(){                           \
+  \       return window.describe(description, fn);   \  
+  \     };                                           \
+  \   };                                             \
+  \ }" :: forall e a. String -> Eff e a -> Eff (describe :: Describe | e) {}
+
+foreign import data It :: !
+
+foreign import it 
+  " function it(description){                \
+  \   return function(fn){                   \
+  \     return function(){                   \
+  \       return window.it(description, fn); \
+  \     };                                   \
+  \   };                                     \
+  \ }" :: forall e a. String -> Eff e a -> Eff (it :: It | e) {}
--- tests/Presentable/Router.spec.purs
+++ tests/Presentable/Router.spec.purs
@@ -2,10 +2,16 @@ module Main where
 
 import Debug.Trace
 import Presentable.Router
+import Control.Monad.Eff
 import Test.Mocha
 import Test.QuickCheck
 
 main = do
-  describe $ do
-    trace "what"
-    quickCheck $ \n -> foozle n == "In Port Foozle " ++ (show n) ++ " eats you!"
+  describe "its a thing!" $ do
+    it "seriously its a thing" $ do
+      quickCheck $ \n -> foozle n == "In Port Foozle " ++ (show n) ++ " eats you!"
+
+    it "no no seriously grue's exist and are out to get you" $ do
+      quickCheck $ \n -> foozle n == "In Port Foozle " ++ (show n) ++ " eats you!"      
+
+    
