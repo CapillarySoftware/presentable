b509b250f2f639150bcdc5b88af4a28840514ae4
Isaac Shapira <fresheyeball@gmail.com>
2014-07-28 16:15:26 -0600

    arg

--- src/Presentable/Router.purs
+++ src/Presentable/Router.purs
@@ -3,18 +3,26 @@ module Presentable.Router where
 import Control.Monad.Eff 
 import Data.Tuple
 import Data.Array
+import Data.Maybe
 import Debug.Foreign 
 import History
 import Control.Reactive
+import Control.Monad.Eff.Exception
 
 type Url  = String
 type View = String
 
+defaultRoute :: [(Tuple Url View)]
+defaultRoute routes = case head routes of 
+  Nothing -> throwException "Your Routes are empty"
+  Just r  -> return r 
+
 -- route :: forall a eff. [(Tuple Url View)] -> Eff ( reactive :: Reactive, history :: History | eff) a
-route routes = do subscribeStateChange \e -> do 
+route routes = do subscribeStateChange \e -> do
   s <- getState
-  let match = filter (\x -> fst x == s.url) routes
-  fprint match
+  d <- defaultRoute routes
+  case filter (\x -> fst x == s.url) routes of
+    [] -> replaceState { title : "", url : (fst d), "data" : {}}
 
   
 
