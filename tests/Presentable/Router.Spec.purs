8aa9e9bc700695065dcaa902659cf50cf7939229
Isaac Shapira <fresheyeball@gmail.com>
2014-07-29 23:03:42 -0600

    sensible

--- tests/Presentable/Router.Spec.purs
+++ tests/Presentable/Router.Spec.purs
@@ -28,7 +28,7 @@ spec = describe "Router" $ do
       let s = unwrapEventDetail e
       case fst <$> head sampleRoutes of
         Just a | a == s.state.url -> return $ itIs done
-        Just a -> return $ expect a `toEqual` s.state.url
+        Just a -> return $ expect a `toNotEqual` s.state.url
 
     route sampleRoutes
     pushState { title : "", url : "/notOnTheList", "data" : {} }
