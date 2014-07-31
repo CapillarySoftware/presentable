5a75eed364d8d9b2b4d6ac6054fc42d2668bda37
Isaac Shapira <fresheyeball@gmail.com>
2014-07-31 15:41:05 -0600

    wtf? why is it nulling my function

--- src/Presentable/ViewParser.purs
+++ src/Presentable/ViewParser.purs
@@ -2,27 +2,36 @@ module Presentable.ViewParser where
 
 import qualified Data.Map as M
 import Data.Either
+import Data.Maybe
 import Data.Foreign
 import Data.Foreign.YAML
 import Control.Reactive
 import Control.Monad.Eff
-import Control.Monad.ST
 import Debug.Foreign
+import Debug.Trace
 
 type Linker a b eff = ((RVar a) -> Eff (reactive :: Reactive | eff) Unit)
 
 data View           = View [String]
 
-instance readView :: ReadForeign View where
+instance readView   :: ReadForeign View where
   read = View <$> prop "View"
 
-present           :: forall a b eff. 
-                     String -> 
-                     (Linker a b eff) -> 
-                     (M.Map String (Linker a b eff)) -> 
-                     (M.Map String (Linker a b eff))
-present     n f m = M.insert n f m
-  
+present             :: forall a b eff. 
+                       String -> 
+                       (Linker a b eff) -> 
+                       (M.Map String (Linker a b eff)) -> 
+                       (M.Map String (Linker a b eff))
+present       n f m = M.insert n f m
 
-view       p yaml = case parseYAML yaml of
-  Right (View a) -> fprint a
+link                :: forall a b efff eff. 
+                       [String] -> 
+                       (M.Map String (Linker a b eff)) -> 
+                       [(Maybe (Eff (trace :: Trace | efff) Unit))]
+link           xs m = (\s -> fprint <$> M.lookup s m) <$> xs
+
+view         m yaml = case parseYAML yaml of
+  Right (View xs) -> do
+    ftrace m
+    ftrace $ M.lookup "Foo" m
+    -- ftrace $ link xs m
