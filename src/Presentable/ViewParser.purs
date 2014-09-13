cc28473a3dc0beae495a762208900a9bc6b2142e
Isaac Shapira <fresheyeball@gmail.com>
2014-09-13 01:32:26 -0600

    render progress

--- src/Presentable/ViewParser.purs
+++ src/Presentable/ViewParser.purs
@@ -6,15 +6,17 @@ import Data.Maybe
 import Data.Function
 import Data.Foreign
 import Data.Traversable
+import Data.Foldable
 import Control.Monad.Eff
 import Control.Monad.Eff.Exception
 import Debug.Foreign
 import Debug.Trace
-import Control.Bind
+import Control.Bind((=<<))
 
-type Yaml       = String
-type Registry a = M.Map String a
-type Wrap a     = [Presentable a]
+type Yaml         = String
+type Registry a   = M.Map String a
+type Wrap a       = [Presentable a]
+type Linker a e b = Maybe a -> Eff e b
 
 data Presentable a
   = Node a
@@ -23,18 +25,22 @@ data Presentable a
 
 throw = throwException <<< error
 
-findLinker :: forall a e. Registry a -> String -> Eff (err :: Exception | e) (Presentable a)
-findLinker r s = case M.lookup s r of 
+makeNode :: forall a e. Registry a -> String -> Eff (err :: Exception | e) (Presentable a)
+makeNode r s = case M.lookup s r of 
   Nothing -> throw $ s ++ " not found in registry"
   Just a  -> return $ Node a
 
 parse :: forall a e. Foreign -> Registry a -> Eff (err :: Exception | e) (Presentable a)
 parse x r = if isArray x 
-            then return <<< Wrap =<< (sequence $ findLinker r <$> unsafeFromForeign x)
-            else findLinker r $ unsafeFromForeign x
+            then return <<< Wrap =<< (sequence $ makeNode r <$> unsafeFromForeign x)
+            else makeNode r <<< unsafeFromForeign $ x
 
 present :: forall a. String -> a -> Registry a -> Registry a
 present = M.insert
+
+render :: forall a b e. Maybe a -> Presentable (Linker a e b) -> Eff e b
+render Nothing (Node f)  = f Nothing
+render Nothing (Wrap xs) = sequence $ render Nothing <$> xs 
 
 emptyRegistery :: forall a. Registry a
 emptyRegistery = M.empty
@@ -49,5 +55,5 @@ yamlToView :: Yaml -> Either String Foreign
 yamlToView = runFn3 parseYamlImpl Left Right
 
 parseAndRender yaml registry = case yamlToView yaml of
-  Right v  -> fprint "moo" --$ parse v registry
+  Right v  -> fprint =<< parse v registry
   Left err -> throw $ "Yaml view failed to parse : " ++ err
