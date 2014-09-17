c3e0bccddf5bf69809cb0e9aadf02a0c3fbb7f13
Isaac Shapira <fresheyeball@gmail.com>
2014-09-17 00:55:31 -0600

    testing

--- tests/Presentable/ViewParser.Spec.purs
+++ tests/Presentable/ViewParser.Spec.purs
@@ -4,7 +4,31 @@ import qualified Data.Map as M
 import Presentable.ViewParser
 import Test.Mocha
 import Test.Chai
+import Test.QuickCheck
+import Data.Maybe
 import Debug.Trace
 import Control.Monad.Eff
+import Control.Monad.ST
 
-spec = trace "moo"
+sampleYaml = 
+  "- header:\n\
+  \    attributes:\n\
+  \      foo : 'foo'\n\
+  \    children:\n\
+  \      - logo\n\
+  \- footer"
+
+spec = describe "ViewParser" $ do
+
+  itAsync "Top level items fire with registered function" $ \done -> do 
+    -- quickCheck $ \name -> do 
+    --   ret <- newSTRef false
+    --   renderYaml ("- " ++ name) $ register name (\_ _ -> writeSTRef ret true) emptyRegistery
+
+    renderYaml "- item" $ register "item" (item done) emptyRegistery
+    where
+    item done' _ _ = do
+      itIs done'
+      return Nothing
+
+-- spec = trace "moo"
