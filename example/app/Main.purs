c21fde1a310a1f156e238a287ef2fd3501cbfc68
Isaac Shapira <fresheyeball@gmail.com>
2014-09-18 23:29:50 -0600

    clickable link!

--- example/app/Main.purs
+++ example/app/Main.purs
@@ -2,6 +2,7 @@ module Main where
 
 import Presentable.ViewParser
 import Presentable.Router
+import History
 import Data.Either
 import Data.Tuple
 import Data.Maybe
@@ -17,29 +18,51 @@ sampleYaml =
   \      - logo\n\
   \- footer"
 
+sampleYamlAbout = 
+  "- header:\n\
+  \    attributes:\n\
+  \      title : 'Presentable / About'\n\
+  \    children:\n\
+  \      - logo\n\
+  \- footer"
+
+render item = ready $ do i <- item
+                         b <- body
+                         append i b
+                         return i
+clearFrame = body >>= clear
+
 header _ (Just a) = do
-  ready $ do h <- createHeader
-             b <- body
-             append h b 
-  return $ Just { bar : "Bar"}
+  render $ create ("<header>"++ a.title ++"</header>") >>= css style
+  return $ Just { src : "http://static.giantbomb.com/uploads/original/1/17172/1419618-unicorn2.jpg" }
   where 
-  createHeader = create ("<header>"++ a.title ++"</header>") 
-    >>= css { top         : 0
-            , left        : 0
-            , position    : "fixed"
-            , width       : "100%"
-            , height      : "50px"
-            , color       : "white"
-            , background  : "black"}
+  style =  { top         : 0
+           , left        : 0
+           , position    : "fixed"
+           , width       : "100%"
+           , padding     : "10px 50px"
+           , fontFamily  : "sans-serif"
+           , color       : "white"
+           , zIndex      : 0
+           , background  : "black"}
 
 footer _ _ = do
   trace "render footer"
   return Nothing
 
-logo   p _ = do 
-  trace "render logo"
-  fprint p
+logo (Just p) _ = do 
+  render $ create ("<img src='" ++ p.src ++ "' />") 
+    >>= css style
+    >>= on "click" \_ _ -> do
+      clearFrame  
+      pushState {url : "/about", title : "about", "data" :{}}
   return Nothing
+  where
+  style =  { top      : 10
+           , left     : 10
+           , zIndex   : 1
+           , height   : 18
+           , position : "fixed"}
 
 main = do
   route rs $ flip renderYaml
@@ -49,4 +72,4 @@ main = do
     $ emptyRegistery
   initRoutes
   where rs = [ (Tuple {url : "/index", title : "home",  "data" :{}} sampleYaml)
-             , (Tuple {url : "/about", title : "about", "data" :{}} sampleYaml)]
+             , (Tuple {url : "/about", title : "about", "data" :{}} sampleYamlAbout)]
