2b24c873d2e3ca980d14d01b78702288ba72cb95
Isaac Shapira <fresheyeball@gmail.com>
2014-09-28 14:24:33 -0600

    version fixed

--- src/Presentable/ViewParser.purs
+++ src/Presentable/ViewParser.purs
@@ -1,44 +1,126 @@
-module Presentable.ViewParser where
+module Presentable.ViewParser 
+  ( Yaml(..), Registry(..), Attributes(..), Linker(..), Presentable(..)
+  , renderYaml, register, emptyRegistery
+  ) where
 
 import qualified Data.Map as M
 import Data.Either
 import Data.Maybe
+import Data.Function
 import Data.Foreign
-import Data.Foreign.YAML
-import Control.Reactive
+import Data.Traversable
+import Data.Foldable
 import Control.Monad.Eff
 import Control.Monad.Eff.Exception
+import Debug.Trace 
 import Debug.Foreign
-import Debug.Trace
 
-type Linker a b eff = ((RVar a) -> Eff (reactive :: Reactive, 
-                                        err      :: Exception | eff) Unit)
+type Yaml              = String
+type Registry a p e    = M.Map String (Linker a p e)
+type Attributes a      = Maybe { | a}
+type Parent p          = Maybe { | p}
+type Linker a p e      = Parent p -> Attributes a -> Eff e (Parent p)
 
-data View           = View [String]
+data Presentable a p e = Presentable (Linker a p e) (Attributes a) (Maybe [Presentable a p e])
 
-throwError          = error >>> throwException
+--
+-- —— Registery ——
+--
 
-instance readView   :: ReadForeign View where
-  read = View <$> prop "View"
+register :: forall a p e. String -> Linker a p e -> Registry a p e -> Registry a p e
+register = M.insert
 
-present             = M.insert
+emptyRegistery :: forall a p e. Registry a p e
+emptyRegistery = M.empty
 
-render              :: forall a b eff. [(Maybe (Linker Number b eff))] -> 
-                          Eff (reactive :: Reactive, 
-                               err      :: Exception | eff) Unit
+--
+-- —— Run Time Checks ——
+--
 
-render     [Just a] = newRVar 0 >>= a
-render    [Nothing] = throwError "Linker not found"
-render       (a:as) = do
-  render [a]
-  render as
+throw = throwException <<< error
 
-view                :: forall a eff. 
-                       M.Map String (Linker Number a eff) -> 
-                       String -> 
-                       Eff (reactive :: Reactive, 
-                            err      :: Exception | eff) Unit
+foreign import isString 
+  "function isString(x){\
+  \ return (typeof x === 'string');\
+  \}" :: Foreign -> Boolean
 
-view         m yaml = case parseYAML yaml of
-  Left err         -> throwError $ "yo yaml, it failed to parse" ++ err
-  Right (View xs)  -> render $ (flip M.lookup) m <$> xs
+foreign import unsafeToString
+  "function unsafeToString(s){ return s; }" :: Foreign -> String
+
+foreign import getNameImpl   
+  "function getNameImpl(x){ return Object.keys(x)[0]; }" :: Foreign -> String  
+getName node = if isString node then unsafeToString node else getNameImpl node
+
+foreign import getAttributesImpl
+  "function getAttributesImpl(Just, Nothing, x){\
+  \ if(!isString(x) && x[getName(x)] && x[getName(x)].attributes){\
+  \   return Just(x[getName(x)].attributes);\
+  \ }else{ return Nothing; }\
+  \}" :: forall a b. Fn3 (a -> Maybe a) (Maybe a) Foreign (Maybe { | b})
+getAttributes :: forall a. Foreign -> Attributes a
+getAttributes = runFn3 getAttributesImpl Just Nothing
+
+foreign import getChildrenImpl
+  "function getChildrenImpl(Just, Nothing, x){\
+  \ if(!isString(x) && x[getName(x)] && x[getName(x)].children){\
+  \   return Just(x[getName(x)].children);\
+  \ }else{ return Nothing; }\
+  \}" :: Fn3 (Foreign -> Maybe Foreign) (Maybe Foreign) Foreign (Maybe [Foreign])
+getChildren :: Foreign -> Maybe [Foreign]
+getChildren = runFn3 getChildrenImpl Just Nothing
+
+--
+-- —— From Foreign to Presentables ——
+--
+
+makePresentable :: forall a p e. Registry a p e -> Foreign -> Eff (err :: Exception | e) (Presentable a p e)
+makePresentable r node = let 
+  returnP l            = Presentable l (getAttributes node)
+  handleC l Nothing    = return $ returnP l Nothing 
+  handleC l (Just ns)  = traverse (makePresentable r) ns >>= Just >>> returnP l >>> return
+  name                 = getName node  
+  in case M.lookup name r of
+    Nothing -> throw $ name ++ " not found in registry"
+    Just l  -> handleC l $ getChildren node        
+
+parse :: forall a p e. Foreign -> Registry a p e-> Eff (err :: Exception | e) [Presentable a p e]
+parse x r = parse' >>= \p -> case p of 
+  Left ns -> return ns 
+  Right n -> return [n]
+  where -- I hear by dub thee, the "run time has no type checks" hack of elegance
+  parse' = if isArray x
+           then traverse (makePresentable r) (unsafeFromForeign x) >>= Left  >>> return
+           else           makePresentable r  (unsafeFromForeign x) >>= Right >>> return
+
+--
+-- —— From Presentables to Render ——
+--
+
+render :: forall a p e. [Presentable a p e] -> Eff e Unit
+render ns = let
+  r :: forall a p e. Parent p -> Presentable a p e -> Eff e (Parent p)
+  r mc (Presentable l a Nothing)   = l mc a -- Execute the Linker, entry point for components
+  r mc (Presentable l a (Just ps)) = do -- Walk the children and fire all Linkers
+    mc' <- r mc (Presentable l a Nothing)
+    traverse (r mc') ps -- Recusively excute all child linkers passing parent return
+    return mc'
+  in traverse_ (r Nothing) ns
+
+--
+-- —— From a Yaml to Render ——
+--
+
+foreign import parseYaml
+  "function parseYaml (left, right, yaml){\
+  \   try{ return right(jsyaml.safeLoad(yaml)); }\
+  \   catch(e){ return left(e.toString()); }\
+  \}" :: forall a. Fn3 (String -> a) (Foreign -> a) Yaml a
+
+renderYaml :: forall a p e. 
+  Yaml -> Registry p a (err :: Exception | e) -> Eff (err :: Exception | e) Unit
+renderYaml yaml r = case yamlToForeign yaml of
+  Right v  -> parse v r >>= render    
+  Left err -> throw $ "Yaml view failed to parse : " ++ err
+  where
+  yamlToForeign :: Yaml -> Either String Foreign
+  yamlToForeign = runFn3 parseYaml Left Right
--- tests/Presentable/Router.Spec.purs
+++ tests/Presentable/Router.Spec.purs
@@ -10,50 +10,45 @@ import Data.Maybe
 import Control.Reactive.Timer
 import Control.Reactive.Event
 
-toState         u = { title : "", url : u, "data" : {} }
-eventUnwrapUrl    :: forall a b. Event (state :: (State b) | a) -> Url
-eventUnwrapUrl  e = (unwrapEventDetail e).state.url
-routeUnwrapUrl    :: forall a.   Route a -> Url
-routeUnwrapUrl  s = (fst s).url
+toState u = { title : "", url : u, "data" : {} }
+eventUnwrapUrl :: forall a b. Event (state :: (State b) | a) -> Url
+eventUnwrapUrl e = (unwrapEventDetail e).state.url
+routeUnwrapUrl :: forall a.   Route a -> Url
+routeUnwrapUrl s = (fst s).url
 
-sampleRoutes      = [ (Tuple { url : "/index", title : "home",     "data" : { }} 
-                             "views/index.yaml")
-                    , (Tuple { url : "/fooo",  title : "foo page", "data" : { }} 
-                             "views/foo.yaml")
-                    , (Tuple { url : "/barr",  title : "bar page", "data" : { }} 
-                             "views/bar.yaml") ]
+sampleRoutes = [ (Tuple { url : "/index", title : "home",     "data" : {}} "views/index.yaml")
+               , (Tuple { url : "/fooo",  title : "foo page", "data" : {}} "views/foo.yaml")
+               , (Tuple { url : "/barr",  title : "bar page", "data" : {}} "views/bar.yaml") ]
 
-testRoute url r done = case r of
-  Just a -> do
-    sub' <- route sampleRoutes   \v -> expect (snd a) `toEqual` v 
-    sub  <- subscribeStateChange \e -> 
-      if   routeUnwrapUrl a == eventUnwrapUrl e
-      then return $ itIs done
-      else return $ expect (routeUnwrapUrl a) `toNotEqual` eventUnwrapUrl e
+testRoute url (Just a) done = do
+  sub' <- route sampleRoutes   \v -> expect (snd a) `toEqual` v
+  sub  <- subscribeStateChange \e ->
+    if   routeUnwrapUrl a == eventUnwrapUrl e
+    then itIs done
+    else expect (routeUnwrapUrl a) `toNotEqual` eventUnwrapUrl e
 
-    pushState <<< toState $ url
+  pushState <<< toState $ url
 
-    -- clean up for the next test
-    unsubscribe sub
-    unsubscribe sub'
+  -- clean up for the next test
+  unsubscribe sub
+  unsubscribe sub'
 
 spec = describe "Router" $ do
 
   beforeEach <<< replaceState <<< toState $ "/before"
 
   itAsync "should default to the first of the list"
-    $ testRoute "/notOnTheList"  
+    $ testRoute "/notOnTheList"
     $ head sampleRoutes
 
-  itAsync "should find middle route"
-    $ testRoute "/fooo" 
-    $ tail sampleRoutes >>= head
+  -- itAsync "should find middle route"
+  --   $ testRoute "/fooo"
+  --   $ tail sampleRoutes >>= head
 
-  itAsync "should find end route" 
-    $ testRoute "/barr" 
+  itAsync "should find end route"
+    $ testRoute "/barr"
     $ last sampleRoutes
 
-  itAsync "should find the first route" 
+  itAsync "should find the first route"
     $ testRoute "/index"
     $ head sampleRoutes
-
